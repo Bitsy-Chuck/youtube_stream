556ea8d80b8e0bcb8290e185ebe875672363a3dc
Ojasv Singh <40452152+Bitsy-Chuck@users.noreply.github.com>
2021-12-26 15:43:39 +0530

    Remove API key from 1.sql

--- public/1.sql
+++ public/1.sql
@@ -1,2 +1,2 @@
 INSERT INTO key_metadata(api_key, config, max_quota, consumed_quota)
-values ('AIzaSyA8X2GRCuphXvyDp5j_Uep4Abv9cNQz0PM', '{"account":"singhtejasv9@gmail.com"}', 1000, 4);
+values ('API_KEY', '{"account":"singhtejasv9@gmail.com"}', 1000, 4);
